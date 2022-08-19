e00a90c38e2142e874e9409dd7ce23c457e51ab6
Lucas <lucas.popescu41@gmail.com>
2022-08-20 00:53:43 +0300

    o incercare de program

---
+++ src/components/herocursa.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hero-wrapper">
+        <div class="hero-stanga">
+            <div class="hero-info">
+                <p class="hero-runda">{{runda}}</p>
+                <div class="steag-locatie">
+                    <div class="steag-wrapper">
+                        <img class="hero-steag" :src="steag">
+                    </div>
+                    <p class="hero-locatie">{{tara}}</p>
+                </div>
+            </div>
+            <div class="hero-circuit">
+                <img class="circuit-image" :src="pozaHarta">
+            </div>
+        </div>
+        <div class="hero-dreapta">
+            <div class="image-wrapper">
+                <img class="hero-image" :src="imagineMare">
+            </div>
+            <div class="hero-data">
+                <p class="data-paragraf">{{dataInceput}}-{{dataSfarsit}} {{lunaCursa}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["dataInceput", "dataSfarsit", "lunaCursa", "pozaHarta", "imagineMare", "runda", "steag", "tara"],
+}
+</script>
+
+<style>
+    @import "./css/hero-cursa.css";
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,64 @@
 <template>
-    <div class="stiri" :class="{darkmode: darkMode}">
-        <div class="titlu">
-            <div>
-                <img src="/checker.webp" class="titlu-poza">
-            </div>
-            <div class="titlu-text">
-                Știri de ultimă oră
-            </div>
-            <div>
-                <img src="/checker.webp" class="titlu-poza">
-            </div>
-        </div>
-    </div>
-    <div class="scroll-btns">
-        <button class="darkmodeBtn" @click="darkModeToggle()">
-            <img src="/night-mode.png" class="poza1" :class="{darkmode: darkMode}">
-            <img src="/brightness.png" class="poza2" :class="{darkmode: darkMode}">
-        </button>
-    </div>
-    <div class="content-grid" :class="{darkmode: darkMode}">
-        <a :href="stire.link" v-for="stire in news" v-bind:key="stire.id" class="ltag" :class="{darkmode: darkMode}">
-            <div class="stire">
-                <div class="content-row">
-                    <div class="content-text" :class="{darkmode: darkMode}">
-                        <p class="text" id="stiretext">{{stire.titlu}}</p>
-                    </div>
-                    <div class="content-photo">
-                        <img :src="stire.poza" class="photo" id="stirephoto">
-                    </div>
+    <div class="site-wrapper">
+        <br>
+        <herocursa v-show="Hero" :dataInceput="dataInceput" :dataSfarsit="dataSfarsit" :lunaCursa="lunaCursa" :pozaHarta="pozaHarta" :imagineMare="imagineMare" :runda="runda" :steag="steag" :tara="tara"/>
+        <div class="stiri" :class="{darkmode: darkMode}">
+            <div class="titlu">
+                <div>
+                    <img src="/checker.webp" class="titlu-poza">
+                </div>
+                <div class="titlu-text">
+                    Știri de ultimă oră
+                </div>
+                <div>
+                    <img src="/checker.webp" class="titlu-poza">
                 </div>
             </div>
-        </a>
+        </div>
+        <div class="scroll-btns">
+            <button class="darkmodeBtn" @click="darkModeToggle()">
+                <img src="/night-mode.png" class="poza1" :class="{darkmode: darkMode}">
+                <img src="/brightness.png" class="poza2" :class="{darkmode: darkMode}">
+            </button>
+        </div>
+        <div class="content-grid" :class="{darkmode: darkMode}">
+            <a :href="stire.link" v-for="stire in news" v-bind:key="stire.id" class="ltag" :class="{darkmode: darkMode}">
+                <div class="stire">
+                    <div class="content-row">
+                        <div class="content-text" :class="{darkmode: darkMode}">
+                            <p class="text" id="stiretext">{{stire.titlu}}</p>
+                        </div>
+                        <div class="content-photo">
+                            <img :src="stire.poza" class="photo" id="stirephoto">
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
     </div>
 </template>
 <script>
+import axios from 'axios'
+import herocursa from "../components/herocursa.vue"
 export default {
     name: "Home",
+    components : {
+        herocursa
+    },
     data () {
         let darkMode = localStorage.getItem('darkMode') == 'true';
         return {
             news: [],
             darkMode,
+            dataInceput: "",
+            dataSfarsit: "",
+            lunaCursa: "",
+            pozaHarta: "",
+            imagineMare: "",
+            runda: "",
+            steag: "",
+            tara: "",
+            Hero: false
         }
     },
     mounted () {
@@ -55,7 +73,11 @@ export default {
             document.body.classList.add("darkmode")
         }else{
             document.body.classList.remove("darkmode")
-        } 
+        }
+        this.getCursa() 
+    },
+    updated () {
+        this.Hero = true
     },
     methods: {
         darkModeToggle() {
@@ -66,7 +88,21 @@ export default {
             }else{
                 document.body.classList.remove("darkmode")
             } 
-        }
+        },
+        async getCursa () {
+            var j=15
+            var link = "https://aqueous-escarpment-24578.herokuapp.com/https://f1scraper44.herokuapp.com/up-next/" + j
+            const response = await axios.get(link)
+            const resData = response.data
+            this.dataInceput = resData.dataCursa1
+            this.dataSfarsit = resData.dataCursa2
+            this.lunaCursa = resData.lunaCursa
+            this.pozaHarta = resData.harta
+            this.imagineMare = resData.imagine
+            this.runda = resData.runda
+            this.steag = resData.steag
+            this.tara = resData.tara
+        },
     },
 }
 </script>
