880294e406cb84648d9257635998adc62f84e7ea
Lucas <lucas.popescu41@gmail.com>
2022-09-11 13:42:18 +0300

    format bine

--- src/views/Program.vue
+++ src/views/Program.vue
@@ -1,7 +1,6 @@
 <template>
     <!-- Cursa Urmatoare -->
     <div class="mt-4 justify-center flex items-center gap-6" alt="hero">
-        <span class="text-4xl font-bold text-red-600">UP</span>
         <div class="border-red-500 border-2 border-solid sm:max-w-xl sm:w-xl lg:max-w-4xl lg:w-4xl p-4 rounded-md w-[50%]">
             <router-link to="/live" style="text-decoration:none; color:black">
                 <div alt="header" class="flex justify-between text-2xl mb-4 font-bold">
@@ -61,7 +60,6 @@
                 </div>
             </router-link>
         </div>
-        <span class="text-4xl font-bold text-red-600">NEXT</span>
     </div>
     <!-- Card Grid -->
     <div alt="card-grid" class="grid lg:grid-cols-2 md:grid-cols-1 lg:px-14 sm:p-6 gap-6 sm:justify-center">
@@ -76,49 +74,49 @@
                         <p class="">FP1</p>
                         <p alt="data" class="">{{new Date(cursa.FirstPractice.date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')}}</p>
                     </div>
-                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.FirstPractice.date + "T" + cursa.FirstPractice.time).toLocaleTimeString('en-US',{ hour12: false }).replace(/:[^:]*/,'')}}</p>
+                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.FirstPractice.date + "T" + cursa.FirstPractice.time).toLocaleTimeString('ro-RO',{ timeStyle: "short" }) + " " + "-" + " " + add1(cursa.FirstPractice.date + "T" + cursa.FirstPractice.time).toLocaleTimeString('ro-RO',{timeStyle: "short"})}}</p>
                 </div>
                 <div alt="Quali" v-if="cursa.Sprint" class="flex justify-between">
                     <div alt="parte-st" class="flex justify-between w-[50%]">
                         <p class="">Quali</p>
                         <p alt="data" class="">{{new Date(cursa.Qualifying.date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')}}</p>
                     </div>
-                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.Qualifying.date + "T" + cursa.Qualifying.time).toLocaleTimeString('en-US',{ hour12: false }).replace(/:[^:]*/,'')}}</p>
+                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.Qualifying.date + "T" + cursa.Qualifying.time).toLocaleTimeString('ro-RO',{timeStyle: "short"}) + " " + "-" + " " + add1(cursa.Qualifying.date + "T" + cursa.Qualifying.time).toLocaleTimeString('ro-RO',{timeStyle: "short"})}}</p>
                 </div>
                 <div alt="FP2" class="flex justify-between">
                     <div alt="parte-st" class="flex justify-between w-[50%]">
                         <p>FP2</p>
                         <p alt="data">{{new Date(cursa.SecondPractice.date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')}}</p>
                     </div>
-                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.SecondPractice.date + "T" + cursa.SecondPractice.time).toLocaleTimeString('en-US',{ hour12: false }).replace(/:[^:]*/,'')}}</p>
+                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.SecondPractice.date + "T" + cursa.SecondPractice.time).toLocaleTimeString('ro-RO',{timeStyle: "short"}) + " " + "-" + " " + add1(cursa.SecondPractice.date + "T" + cursa.SecondPractice.time).toLocaleTimeString('ro-RO',{timeStyle: "short"})}}</p>
                 </div>
                 <div alt="Sprint" v-if="cursa.Sprint" class="flex justify-between">
                     <div alt="parte-st" class="flex justify-between w-[50%]">
                         <p>Sprint</p>
                         <p alt="data">{{new Date(cursa.Sprint.date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')}}</p>
                     </div>
-                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.Sprint.date + "T" + cursa.Sprint.time).toLocaleTimeString('en-US',{ hour12: false }).replace(/:[^:]*/,'')}}</p>
+                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.Sprint.date + "T" + cursa.Sprint.time).toLocaleTimeString('ro-RO',{timeStyle: "short"}) + " " + "-" + " " + add1(cursa.Sprint.date + "T" + cursa.Sprint.time).toLocaleTimeString('ro-RO',{timeStyle: "short"})}}</p>
                 </div>
                 <div alt="FP3" v-if="cursa.ThirdPractice" class="flex justify-between">
                     <div alt="parte-st" class="flex justify-between w-[50%]">
                         <p>FP3</p>
                         <p alt="data">{{new Date(cursa.ThirdPractice.date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')}}</p>
                     </div>
-                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.ThirdPractice.date + "T" + cursa.ThirdPractice.time).toLocaleTimeString('en-US',{ hour12: false }).replace(/:[^:]*/,'')}}</p>
+                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.ThirdPractice.date + "T" + cursa.ThirdPractice.time).toLocaleTimeString('ro-RO',{timeStyle: "short"}) + " " + "-" + " " + add1(cursa.ThirdPractice.date + "T" + cursa.ThirdPractice.time).toLocaleTimeString('ro-RO',{timeStyle: "short"})}}</p>
                 </div>
                 <div alt="Quali" v-if="!cursa.Sprint" class="flex justify-between">
                     <div alt="parte-st" class="flex justify-between w-[50%]">
                         <p>Quali</p>
                         <p alt="data">{{new Date(cursa.Qualifying.date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')}}</p>
                     </div>
-                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.Qualifying.date + "T" + cursa.Qualifying.time).toLocaleTimeString('en-US',{ hour12: false }).replace(/:[^:]*/,'')}}</p>
+                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.Qualifying.date + "T" + cursa.Qualifying.time).toLocaleTimeString('ro-RO',{timeStyle: "short"}) + " " + "-" + " " + add1(cursa.Qualifying.date + "T" + cursa.Qualifying.time).toLocaleTimeString('ro-RO',{timeStyle: "short"})}}</p>
                 </div>
                 <div alt="Cursa" class="flex justify-between">
                     <div alt="parte-st" class="flex justify-between w-[50%]">
                         <p>Cursă</p>
                         <p alt="data">{{new Date(cursa.date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')}}</p>
                     </div>
-                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.date + "T" + cursa.time).toLocaleTimeString('en-US',{ hour12: false }).replace(/:[^:]*/,'')}}</p>
+                    <p alt="timp" class="w-[50%] flex justify-end items-center">{{new Date(cursa.date + "T" + cursa.time).toLocaleTimeString('ro-RO',{timeStyle: "short"})}}</p>
                 </div>
             </div>
         </div>
@@ -177,29 +175,43 @@ export default {
             const response = await axios.get(link)
             const resData = response.data.MRData.RaceTable.Races
             this.curse = resData
+            console.log(resData);
             this.hero = this.curse[resData1]
 
 
             this.circuitName = this.hero.Circuit.circuitId.charAt(0).toUpperCase() + this.hero.Circuit.circuitId.slice(1).replace(/_/g, ' ')
             this.round = this.hero.round
             this.FP1.date = new Date(this.hero.FirstPractice.date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')
-            this.FP1.time = new Date(this.hero.FirstPractice.date + "T" + this.hero.FirstPractice.time).toLocaleTimeString('en-US',{ hour12: false }).replace(/:[^:]*/,'')
+            this.FP1.time = new Date(this.hero.FirstPractice.date + "T" + this.hero.FirstPractice.time).toLocaleTimeString('ro-RO',{timeStyle: "short"}) + " " + "-" + " " + add1Hour(this.hero.FirstPractice.date + "T" + this.hero.FirstPractice.time).toLocaleTimeString('ro-RO',{timeStyle: "short"})
             this.FP2.date = new Date(this.hero.SecondPractice.date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')
-            this.FP2.time = new Date(this.hero.SecondPractice.date + "T" + this.hero.SecondPractice.time).toLocaleTimeString('en-US',{ hour12: false }).replace(/:[^:]*/,'')
+            this.FP2.time = new Date(this.hero.SecondPractice.date + "T" + this.hero.SecondPractice.time).toLocaleTimeString('ro-RO',{timeStyle: "short"}) + " " + "-" + " " + add1Hour(this.hero.SecondPractice.date + "T" + this.hero.SecondPractice.time).toLocaleTimeString('ro-RO',{timeStyle: "short"})
             if(this.hero.ThirdPractice){
                 this.FP3.date = new Date(this.hero.ThirdPractice.date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')
-                this.FP3.time = new Date(this.hero.ThirdPractice.date + "T" + this.hero.ThirdPractice.time).toLocaleTimeString('en-US',{ hour12: false }).replace(/:[^:]*/,'')
+                this.FP3.time = new Date(this.hero.ThirdPractice.date + "T" + this.hero.ThirdPractice.time).toLocaleTimeString('ro-RO',{timeStyle: "short"}) + " " + "-" + " " + add1Hour(this.hero.ThirdPractice.date + "T" + this.hero.ThirdPractice.time).toLocaleTimeString('ro-RO',{timeStyle: "short"})
             }
             if(this.hero.Sprint){
                 this.Sprint.date = new Date(this.hero.Sprint.date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')
-                this.Sprint.time = new Date(this.hero.Sprint.date + "T" + this.hero.Sprint.time).toLocaleTimeString('en-US',{ hour12: false }).replace(/:[^:]*/,'')
+                this.Sprint.time = new Date(this.hero.Sprint.date + "T" + this.hero.Sprint.time).toLocaleTimeString('ro-RO',{timeStyle: "short"}) + " " + "-" + " " + add1Hour(this.hero.Sprint.date + "T" + this.hero.Sprint.time).toLocaleTimeString('ro-RO',{timeStyle: "short"})
             }
             this.Quali.date = new Date(this.hero.Qualifying.date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')
-            this.Quali.time = new Date(this.hero.Qualifying.date + "T" + this.hero.Qualifying.time).toLocaleTimeString('en-US',{ hour12: false }).replace(/:[^:]*/,'')
+            this.Quali.time = new Date(this.hero.Qualifying.date + "T" + this.hero.Qualifying.time).toLocaleTimeString('ro-RO',{timeStyle: "short"}) + " " + "-" + " " + add1Hour(this.hero.Qualifying.date + "T" + this.hero.Qualifying.time).toLocaleTimeString('ro-RO',{timeStyle: "short"})
             this.Race.date = new Date(this.hero.date).toISOString().replace(/T.*/,'').split('-').reverse().join('-')
-            this.Race.time = new Date(this.hero.date + "T" + this.hero.time).toLocaleTimeString('en-US',{ hour12: false }).replace(/:[^:]*/,'')
+            this.Race.time = new Date(this.hero.date + "T" + this.hero.time).toLocaleTimeString('ro-RO',{timeStyle: "short"})
             this.Name = this.hero.raceName
+
+            function add1Hour(timp1){
+                var test = new Date(timp1)
+                var x = test.getTime()
+                var hour1 = new Date(x + 60 * 60 * 1000)
+                return hour1
+            }
         },
+        add1 (timpinceput) {
+            var test = new Date(timpinceput)
+            var x = test.getTime()
+            var timpscos = new Date(x + 60 * 60 * 1000)
+            return timpscos
+        }
     },
 }
 </script>
