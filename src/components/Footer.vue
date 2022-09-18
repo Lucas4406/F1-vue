fbb02d3dbf4eef054635392f0073d156352ac2f2
Lucas <lucas.popescu41@gmail.com>
2022-09-19 00:11:09 +0300

    aa

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,9 @@
 <template>
-    <div class="bg-black bottom-0 left-0 right-0 text-red-500 mb-0 h-36 flex items-center justify-around flex-row gap-10 text-xl">
+    <div class="bg-black bottom-0 left-0 right-0 text-red-500 mb-0 h-36 flex items-center justify-center flex-row gap-10 text-xl">
         <div>
             <div class="flex flex-row gap-4">
-                <router-link to="/login" class="text-red-500" @click="bla">Log In</router-link>
-                <router-link to="/signup" class="text-red-500" @click="bla">Sign Up</router-link>
-            </div>
-            <div class="flex flex-row gap-4 text-lg">
-                <button @click="logout" v-if="isLoggedIn" class="bg-red-500 text-black hover:scale-105 transition-all duration-200 cursor-pointer h-10">Log out</button>
-                <router-link to="/profile" v-if="isLoggedIn" class="bg-red-500 text-black hover:scale-105 transition-all duration-200 cursor-pointer flex justify-center items-center px-2 h-10" @click="bla">Profil</router-link>
+                <router-link to="/login" class="bg-red-500 text-black px-4 py-2" @click="bla" v-if="!isLoggedIn">Log In</router-link>
+                <router-link to="/signup" class="bg-red-500 text-black px-4 py-2" @click="bla" v-if="!isLoggedIn">Sign Up</router-link>
             </div>
         </div>
         <div v-if="isLoggedIn">
@@ -21,12 +17,14 @@
             </div>
             <div class="sec self-center p-2 w-2/8">
                 <div class="buttons text-xs flex font-light">
-                <div class="btn p-2 mr-1 rounded shadow cursor-pointer hover:bg-gray-100">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                </div>
-                <div class="btn p-2 mr-1 rounded shadow cursor-pointer hover:bg-gray-100">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" /></svg>
-                </div>
+                    <router-link to="/profile" class="btn p-2 mr-1 rounded shadow cursor-pointer hover:bg-gray-100">
+                        <svg class="w-5 h-5" viewBox="0 0 32 32" enable-background="new 0 0 32 32" id="Stock_cut" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><desc/><g><circle cx="16" cy="16" fill="none" r="15" stroke="#000000" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/><path d="M26,27L26,27   c0-5.523-4.477-10-10-10h0c-5.523,0-10,4.477-10,10v0" fill="none" stroke="#000000" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/><circle cx="16" cy="11" fill="none" r="6" stroke="#000000" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/></g></svg>
+                    </router-link>
+                    <div class="btn p-2 mr-1 rounded shadow cursor-pointer hover:bg-gray-100" @click="logout">
+                        <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5">
+                            <path d="M868 732h-70.3c-4.8 0-9.3 2.1-12.3 5.8-7 8.5-14.5 16.7-22.4 24.5a353.84 353.84 0 0 1-112.7 75.9A352.8 352.8 0 0 1 512.4 866c-47.9 0-94.3-9.4-137.9-27.8a353.84 353.84 0 0 1-112.7-75.9 353.28 353.28 0 0 1-76-112.5C167.3 606.2 158 559.9 158 512s9.4-94.2 27.8-137.8c17.8-42.1 43.4-80 76-112.5s70.5-58.1 112.7-75.9c43.6-18.4 90-27.8 137.9-27.8 47.9 0 94.3 9.3 137.9 27.8 42.2 17.8 80.1 43.4 112.7 75.9 7.9 7.9 15.3 16.1 22.4 24.5 3 3.7 7.6 5.8 12.3 5.8H868c6.3 0 10.2-7 6.7-12.3C798 160.5 663.8 81.6 511.3 82 271.7 82.6 79.6 277.1 82 516.4 84.4 751.9 276.2 942 512.4 942c152.1 0 285.7-78.8 362.3-197.7 3.4-5.3-.4-12.3-6.7-12.3zm88.9-226.3L815 393.7c-5.3-4.2-13-.4-13 6.3v76H488c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h314v76c0 6.7 7.8 10.5 13 6.3l141.9-112a8 8 0 0 0 0-12.6z"/>
+                        </svg>
+                    </div>
                 </div>
             </div>
             </div>
--- src/views/profile/profile.vue
+++ src/views/profile/profile.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="w-screen flex justify-center items-center min-h-screen flex-col">
-    <div class="login-box">
+    <!-- Update-box -->
+    <div class="login-box" v-if="updateProf">
         <h2>Adaugă un nume și o poză!</h2>
         <form @submit.prevent="schimba">
             <div class="user-box">
@@ -22,16 +23,64 @@
             <input type="submit" hidden />
         </form>
     </div>
+
+    <!-- Profile-box -->
+    <div class="w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700" v-if="!updateProf">
+      <div class="flex justify-end px-4 pt-4">
+          <button id="dropdownButton" data-dropdown-toggle="dropdown" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
+              <span class="sr-only">Open dropdown</span>
+              <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path></svg>
+          </button>
+          <!-- Dropdown menu -->
+          <div id="dropdown" class="hidden z-10 w-44 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
+              <ul class="py-1" aria-labelledby="dropdownButton">
+              <li>
+                  <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
+              </li>
+              <li>
+                  <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Export Data</a>
+              </li>
+              <li>
+                  <a href="#" class="block py-2 px-4 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
+              </li>
+              </ul>
+          </div>
+      </div>
+      <div class="flex flex-col items-center pb-10">
+          <img class="mb-3 w-24 h-24 rounded-full shadow-lg" :src="Photo" alt="image"/>
+          <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{Name}}</h5>
+          <span class="text-sm text-gray-500 dark:text-gray-400">{{Email}}</span>
+          <div class="flex mt-4 space-x-3 md:mt-6">
+              <button @click="logout" class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer">Log out</button>
+          </div>
+      </div>
+  </div>
 </div>
 </template>
 
 <script setup>
-    import { getAuth, updateProfile } from "firebase/auth"
+    import { getAuth, updateProfile, signOut } from "firebase/auth"
     import {ref} from "vue"
     import { useRouter } from "vue-router"
     const nume = ref("")
     const poza = ref("")
+    const updateProf = ref(false)
+    const Name = ref("")
+    const Photo = ref("")
+    const Email = ref("")
     const router = useRouter()
+    const auth = getAuth()
+    const user = auth.currentUser
+    if(user.displayName === null || user.photoURL === null){
+        updateProf.value = true
+    }else{
+        updateProf.value = false
+    }
+    if(user != null){
+        Name.value = user.displayName
+        Photo.value = user.photoURL
+        Email.value = user.email
+    }
     function schimba () {
         const auth = getAuth()
         updateProfile(auth.currentUser, {
@@ -41,6 +90,11 @@
         })
         .catch((err) => {
             alert(err.message)
+        })
+    }
+    function logout() {
+        signOut(auth).then(() => {
+            router.push("/")
         })
     }
 </script>
