0b0ae2add6daca3e68b9a2958461510ee3ce42f2
Lucas <lucas.popescu41@gmail.com>
2022-10-21 08:23:36 +0300

    mayber?

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
+import topLevelAwait from "vite-plugin-top-level-await"
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    topLevelAwait({
+      promiseExportName: "__tla",
+      promiseImportName: i => `__tla_${i}`
+    })
+
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
