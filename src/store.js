e5e3a758f6c24299d313ce913b794266819a093b
Lucas <lucas.popescu41@gmail.com>
2022-10-21 08:15:39 +0300

    mm

--- src/store.js
+++ src/store.js
@@ -3,12 +3,6 @@ import axios from "axios"
 const currUser = JSON.parse(localStorage.getItem("currentUser"))
 let favD 
 let favT
-await getUserData(currUser)
-const state = reactive({
-    name: currUser,
-    favDriver: favD,
-    favTeam: favT
-})
 
 async function getUserData (user){
     const response = await axios(`https://f1-site-api.vercel.app/profile/${user}`)
@@ -17,6 +11,12 @@ async function getUserData (user){
     favT = data.favTeam
 }
 
+await getUserData(currUser)
+const state = reactive({
+    name: currUser,
+    favDriver: favD,
+    favTeam: favT
+})
 
 export default {
     state: state
