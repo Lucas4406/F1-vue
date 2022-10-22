79e80cb22a09c6eb9f9f80fd08fe18fe3463b0b3
Lucas <lucas.popescu41@gmail.com>
2022-10-23 00:39:53 +0300

    ok?

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -57,24 +57,27 @@
   if(store.state.email != null){
     isLogged.value = true
   }
-   onMounted(() => {
-     document.addEventListener("click", (e) => {
-         const isDropdownButton = e.target.matches("[data-dropdown-button]");
-       if (!isDropdownButton && e.target.closest("[data-dropdown]") != null)
-         return
-       let currentDropdown
-       if (isDropdownButton) {
-           currentDropdown = e.target.closest("[data-dropdown]")
-         currentDropdown.classList.toggle("active")
-       }
-       document
-         .querySelectorAll("[data-dropdown].active")
-         .forEach((dropdown) => {
-             if (dropdown === currentDropdown) return
-           dropdown.classList.remove("active")
-         })
-     })
-   })
+  document.addEventListener("click", (e) => {
+      const isDropdownButton = e.target.matches("[data-dropdown-button]");
+    if (!isDropdownButton && e.target.closest("[data-dropdown]") != null)
+      return
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest("[data-dropdown]")
+      currentDropdown.classList.toggle("active")
+    }
+    document
+      .querySelectorAll("[data-dropdown].active")
+      .forEach((dropdown) => {
+          if (dropdown === currentDropdown) return
+        dropdown.classList.remove("active")
+      })
+  })
+  document
+  .querySelectorAll("[data-dropdown].active")
+  .forEach((dropdown) => {
+    dropdown.classList.remove("active")
+  })
 </script>
 
 <style>
